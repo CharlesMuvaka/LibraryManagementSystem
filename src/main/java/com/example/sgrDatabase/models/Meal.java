102a7ea25a5626c82a87fb626538c1b5d94e0425
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2022-11-11 02:48:02 +0300

    create a blueprint to define a single instance of a Meal

--- src/main/java/com/example/sgrDatabase/models/Meal.java
+++ src/main/java/com/example/sgrDatabase/models/Meal.java
@@ -1,2 +1,59 @@
-package com.example.sgrDatabase.models;public class Meal {
+package com.example.sgrDatabase.models;
+
+public class Meal {
+    private int id;
+    private String name;
+    private String category;
+    private String Origin;
+    private String Description;
+
+    public Meal() {
+    }
+
+    public Meal(String name, String category, String origin, String description) {
+        this.name = name;
+        this.category = category;
+        Origin = origin;
+        Description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getOrigin() {
+        return Origin;
+    }
+
+    public void setOrigin(String origin) {
+        Origin = origin;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
 }
