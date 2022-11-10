d3ed08dcde26532f2f4881359a55fcdcd27e933c
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2022-11-11 02:48:25 +0300

    create a blueprint to define a single instance of a order

--- src/main/java/com/example/sgrDatabase/models/Order.java
+++ src/main/java/com/example/sgrDatabase/models/Order.java
@@ -1,2 +1,48 @@
-package com.example.sgrDatabase.models;public class Order {
+package com.example.sgrDatabase.models;
+
+import java.sql.Timestamp;
+
+public class Order {
+    private int id;
+    private int userId;
+    private int mealId;
+    private Timestamp doneTime;
+
+    public Order(int userId, int mealId, Timestamp doneTime) {
+        this.userId = userId;
+        this.mealId = mealId;
+        this.doneTime = doneTime;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getMealId() {
+        return mealId;
+    }
+
+    public void setMealId(int mealId) {
+        this.mealId = mealId;
+    }
+
+    public Timestamp getDoneTime() {
+        return doneTime;
+    }
+
+    public void setDoneTime(Timestamp doneTime) {
+        this.doneTime = doneTime;
+    }
 }
