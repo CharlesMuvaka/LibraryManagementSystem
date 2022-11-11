ff77f45d68af98c1d3508d0e86945d38cd39c97d
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2022-11-11 04:02:36 +0300

    implement the doneOrder interface to use its method and make the class a service class using the @Service annotation

--- src/main/java/com/example/sgrDatabase/services/DoneOrderService.java
+++ src/main/java/com/example/sgrDatabase/services/DoneOrderService.java
@@ -1,2 +1,36 @@
-package com.example.sgrDatabase.services;public class DoneOrderService {
+package com.example.sgrDatabase.services;
+
+import com.example.sgrDatabase.interfaces.DoneOrderDao;
+import com.example.sgrDatabase.interfaces.UserDao;
+import com.example.sgrDatabase.models.DoneOrder;
+import com.example.sgrDatabase.models.Waiter;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class DoneOrderService implements DoneOrderDao {
+    @Override
+    public void addDoneOrder(DoneOrder doneOrder) {
+
+    }
+
+    @Override
+    public DoneOrder getDoneOrderById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Waiter> getWaitersDoneOrdersById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<DoneOrderDao> getAllDoneOrders() {
+        return null;
+    }
+
+    @Override
+    public List<UserDao> getUsersOrders(int id) {
+        return null;
+    }
 }
