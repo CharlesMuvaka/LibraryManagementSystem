ea53e80f191a8871c50a9e203f16e674ba5e01ff
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2022-11-11 04:52:20 +0300

    inject WorkerRepository instance to arquire all its methods and fields during the classs initialization

--- src/main/java/com/example/sgrDatabase/services/WaiterService.java
+++ src/main/java/com/example/sgrDatabase/services/WaiterService.java
@@ -2,12 +2,19 @@ package com.example.sgrDatabase.services;
 
 import com.example.sgrDatabase.interfaces.Worker;
 import com.example.sgrDatabase.models.Waiter;
+import com.example.sgrDatabase.repositories.WorkerRepo;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class WaiterService implements Worker {
+    private WorkerRepo repo;
+
+    public WaiterService(WorkerRepo repo) {
+        this.repo = repo;
+    }
+
     @Override
     public void addWorker(Waiter waiter) {
 
