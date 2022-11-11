29ab8765181e29da858a3e867440eb4ba50aceef
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2022-11-11 04:15:28 +0300

    create an interface that extends the Jpa Repository to use its available methods to query the database for orders information

--- src/main/java/com/example/sgrDatabase/repositories/OrderRepo.java
+++ src/main/java/com/example/sgrDatabase/repositories/OrderRepo.java
@@ -1,4 +1,7 @@
 package com.example.sgrDatabase.repositories;
 
-public class OrderRepo {
+import com.example.sgrDatabase.models.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepo extends JpaRepository<Order, Integer> {
 }
