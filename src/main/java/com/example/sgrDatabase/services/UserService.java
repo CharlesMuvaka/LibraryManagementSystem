a14d3bf1b7a6836149cfeffe797c6711475a3a2f
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2022-11-11 04:05:22 +0300

    implement the UserDAo interface to use its method and make the class a service class using the @Service annotation

--- src/main/java/com/example/sgrDatabase/services/UserService.java
+++ src/main/java/com/example/sgrDatabase/services/UserService.java
@@ -1,2 +1,25 @@
-package com.example.sgrDatabase.services;public class UserService {
+package com.example.sgrDatabase.services;
+
+import com.example.sgrDatabase.interfaces.UserDao;
+import com.example.sgrDatabase.models.User;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService implements UserDao {
+    @Override
+    public void addUser(User user) {
+
+    }
+
+    @Override
+    public User getUserById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<User> getAlUsers() {
+        return null;
+    }
 }
