67d559fe4028f0fd1bd8c776ddf98292689e5911
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2022-11-11 03:41:34 +0300

    add a method to retrieve a list of all waiters

--- src/main/java/com/example/sgrDatabase/interfaces/Worker.java
+++ src/main/java/com/example/sgrDatabase/interfaces/Worker.java
@@ -2,9 +2,12 @@ package com.example.sgrDatabase.interfaces;
 
 import com.example.sgrDatabase.models.Waiter;
 
+import java.util.List;
+
 public interface Worker {
     //Create
     void addWorker(Waiter waiter);
     //Read
     Waiter getaWaiterById(Waiter waiter);
+    List<Waiter> getWaiters();
 }
