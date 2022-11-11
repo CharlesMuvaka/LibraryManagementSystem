ee7ecc5804d1eda37932dd0c95e4383b38def5ce
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2022-11-11 03:53:21 +0300

    create a method that retrieves all the users

--- src/main/java/com/example/sgrDatabase/interfaces/UserDao.java
+++ src/main/java/com/example/sgrDatabase/interfaces/UserDao.java
@@ -2,9 +2,12 @@ package com.example.sgrDatabase.interfaces;
 
 import com.example.sgrDatabase.models.User;
 
+import java.util.List;
+
 public interface UserDao {
     //Create
     void addUser(User user);
     //Read
     User getUserById(int id);
+    List<User> getAlUsers();
 }
