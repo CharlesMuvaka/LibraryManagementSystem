f4cff1003e6c897861beeec0b2e420b2f25f80dc
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2022-11-11 04:22:46 +0300

    inject DoneOrderRepo instance to use all its attributes and methods

--- src/main/java/com/example/sgrDatabase/services/DoneOrderService.java
+++ src/main/java/com/example/sgrDatabase/services/DoneOrderService.java
@@ -4,14 +4,21 @@ import com.example.sgrDatabase.interfaces.DoneOrderDao;
 import com.example.sgrDatabase.interfaces.UserDao;
 import com.example.sgrDatabase.models.DoneOrder;
 import com.example.sgrDatabase.models.Waiter;
+import com.example.sgrDatabase.repositories.DoneOrderRepo;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 @Service
 public class DoneOrderService implements DoneOrderDao {
+    private DoneOrderRepo repo;
+
+    public DoneOrderService(DoneOrderRepo repo) {
+        this.repo = repo;
+    }
+
     @Override
     public void addDoneOrder(DoneOrder doneOrder) {
-
+        repo.save(doneOrder);
     }
 
     @Override
