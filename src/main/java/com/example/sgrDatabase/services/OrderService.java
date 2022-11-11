6eafaa107d81df17d7685b9d8f9b624c8133b01b
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2022-11-11 04:04:39 +0300

    implement the OrderDAo interface to use its method and make the class a service class using the @Service annotation

--- src/main/java/com/example/sgrDatabase/services/OrderService.java
+++ src/main/java/com/example/sgrDatabase/services/OrderService.java
@@ -1,2 +1,30 @@
-package com.example.sgrDatabase.services;public class OrderService {
+package com.example.sgrDatabase.services;
+
+import com.example.sgrDatabase.interfaces.OrderDao;
+import com.example.sgrDatabase.models.Order;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class OrderService implements OrderDao {
+    @Override
+    public void addOrder(Order order) {
+
+    }
+
+    @Override
+    public Order getOrderById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Order> getAllOrders() {
+        return null;
+    }
+
+    @Override
+    public List<Order> getOrdersByUser(int id) {
+        return null;
+    }
 }
