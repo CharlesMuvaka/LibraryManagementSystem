66b859de4d5841a69de1f5cdad40e448ddf062a4
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2022-11-11 04:06:17 +0300

    implement the Worker interface to use its method and make the class a service class using the @Service annotation

--- src/main/java/com/example/sgrDatabase/services/WaiterService.java
+++ src/main/java/com/example/sgrDatabase/services/WaiterService.java
@@ -1,2 +1,25 @@
-package com.example.sgrDatabase.services;public class WaiterService {
+package com.example.sgrDatabase.services;
+
+import com.example.sgrDatabase.interfaces.Worker;
+import com.example.sgrDatabase.models.Waiter;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class WaiterService implements Worker {
+    @Override
+    public void addWorker(Waiter waiter) {
+
+    }
+
+    @Override
+    public Waiter getaWaiterById(Waiter waiter) {
+        return null;
+    }
+
+    @Override
+    public List<Waiter> getWaiters() {
+        return null;
+    }
 }
