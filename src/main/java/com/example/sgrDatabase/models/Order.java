a2921abf1613317e4838bf672ba951c198399b1d
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2022-11-11 03:09:54 +0300

    create a sequence geerator to define the initial value and allocation size of the id

--- src/main/java/com/example/sgrDatabase/models/Order.java
+++ src/main/java/com/example/sgrDatabase/models/Order.java
@@ -7,6 +7,12 @@ import java.sql.Timestamp;
 public class Order {
     @Id
     @Column(name = "user_id")
+    @SequenceGenerator(name = "order_sequence",
+            sequenceName = "order_sequence",
+            initialValue = 1,
+            allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,
+            generator = "order_sequence")
     private int id;
     private int userId;
     private int mealId;
