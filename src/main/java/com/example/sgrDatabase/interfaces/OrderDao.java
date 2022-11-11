67efe0edf7238f9e4212bbb97873261b8182017b
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2022-11-11 03:46:38 +0300

    create a method that retrieves all orders

--- src/main/java/com/example/sgrDatabase/interfaces/OrderDao.java
+++ src/main/java/com/example/sgrDatabase/interfaces/OrderDao.java
@@ -2,9 +2,12 @@ package com.example.sgrDatabase.interfaces;
 
 import com.example.sgrDatabase.models.Order;
 
+import java.util.List;
+
 public interface OrderDao {
     //Create
     void addOrder(Order order);
     //Read
     Order getOrderById(int id);
+    List<Order> getAllOrders();
 }
