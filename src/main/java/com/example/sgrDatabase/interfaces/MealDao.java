59adf6e1bc083a571253f2f50741a824cfe37133
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2022-11-11 03:57:50 +0300

    create a method that retrieves all meals

--- src/main/java/com/example/sgrDatabase/interfaces/MealDao.java
+++ src/main/java/com/example/sgrDatabase/interfaces/MealDao.java
@@ -2,9 +2,12 @@ package com.example.sgrDatabase.interfaces;
 
 import com.example.sgrDatabase.models.Meal;
 
+import java.util.List;
+
 public interface MealDao {
     //Create
     void addMeal(Meal meal);
     //Read
     Meal getMealById(int id);
+    List<Meal> getAllMeals();
 }
