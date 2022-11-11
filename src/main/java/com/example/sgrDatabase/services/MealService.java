2c8f0a560aff8b09b02dcdc82bdd833779c66213
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2022-11-11 04:03:49 +0300

    implement the MealDAo interface to use its method and make the class a service class using the @Service annotation

--- src/main/java/com/example/sgrDatabase/services/MealService.java
+++ src/main/java/com/example/sgrDatabase/services/MealService.java
@@ -1,4 +1,30 @@
 package com.example.sgrDatabase.services;
 
-public class MealService {
+import com.example.sgrDatabase.interfaces.MealDao;
+import com.example.sgrDatabase.models.Meal;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MealService implements MealDao {
+    @Override
+    public void addMeal(Meal meal) {
+
+    }
+
+    @Override
+    public Meal getMealById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Meal> getAllMeals() {
+        return null;
+    }
+
+    @Override
+    public List<Meal> findByCategory() {
+        return null;
+    }
 }
