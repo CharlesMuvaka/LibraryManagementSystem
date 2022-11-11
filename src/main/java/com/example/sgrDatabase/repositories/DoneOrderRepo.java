725c6a03debd3ef713e3dc5daa63a424b188ba8b
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2022-11-11 04:16:14 +0300

    create an interface that extends the Jpa Repository to use its available methods to query the database for done Orders information

--- src/main/java/com/example/sgrDatabase/repositories/DoneOrderRepo.java
+++ src/main/java/com/example/sgrDatabase/repositories/DoneOrderRepo.java
@@ -1,2 +1,7 @@
-package com.example.sgrDatabase.repositories;public class DoneOrderRepo {
+package com.example.sgrDatabase.repositories;
+
+import com.example.sgrDatabase.models.DoneOrder;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DoneOrderRepo extends JpaRepository<DoneOrder, Integer> {
 }
