0e1013d587e7b09937ca4e0245ba292511be8fea
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2022-11-11 04:49:19 +0300

    inject userRepository instance to arquire all its methods and fields during the classs initialization

--- src/main/java/com/example/sgrDatabase/services/UserService.java
+++ src/main/java/com/example/sgrDatabase/services/UserService.java
@@ -2,15 +2,22 @@ package com.example.sgrDatabase.services;
 
 import com.example.sgrDatabase.interfaces.UserDao;
 import com.example.sgrDatabase.models.User;
+import com.example.sgrDatabase.repositories.UserRepo;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class UserService implements UserDao {
+    private UserRepo repo;
+
+    public UserService(UserRepo repo) {
+        this.repo = repo;
+    }
+
     @Override
     public void addUser(User user) {
-
+        repo.save(user);
     }
 
     @Override
